435c6253fc140915dc621177dd4e7e84252de776
Marcus Vinicius <5ucr4m@gmail.com>
2020-07-10 13:40:14 -0300

    push

--- api/operations/controllers/operations.js
+++ api/operations/controllers/operations.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async find(ctx) {
+    const { id } = ctx.state.user;
+    let entities = await strapi.services.operations.find();
+    entities = entities.filter((entity) => entity.user.id === id);
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.operations })
+    );
+  },
+};
